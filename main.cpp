ed3579e31c18a1b58f31473765c16c3e7266ac9f
starsharp06sharp <zhenglei@std.uestc.edu.cn>
2015-05-21 19:46:11 +0800

    v0.10c
    Simplified lots of code

--- main.cpp
+++ main.cpp
@@ -8,18 +8,11 @@
 #include "Plane.h"
 #include "Bullet.h"
 
-//sf::Vector2f
-//getStepFromKeyboardEvent(
-//    sf::Event keyEvent
-//    );
-template < typename T>
-void flashIt (T& it) {
-    it.flash();
-}
 
-int main() {
+int main( void ) {
     //Create the main window
     sf::RenderWindow windowMain(sf::VideoMode(240, 400), "HitPlane By L.Zheng");
+    windowMain.setFramerateLimit(60);
 
     //Load background from file
     sf::Texture textureBackground;
@@ -55,47 +48,36 @@ int main() {
         3
         );
     //TEMP player's bullet
-    std::list<Bullet*> playerBullet;
+    std::list<Bullet> playerBullet;
 
     unsigned NUM=0;
     bool shot;
     //Main loop
     while (windowMain.isOpen()) {
         sf::Event event;
+
         while (windowMain.pollEvent(event)) {
             //Close window when exit
             if (event.type == sf::Event::Closed) {
                 windowMain.close();
             }
-
-            /*
-            //Move player's plane when key preessed
-            if (event.type == sf::Event::KeyPressed) {
-                player.move(
-                    getStepFromKeyboardEvent( event )
-                    );
-                sf::FloatRect gBound = player.getGlobalBounds();
-                //std::cout<<'('<<gBound.left<<','<<gBound.top<<')'
-                //<<','<<'('<<gBound.left+gBound.width<<','<<gBound.top+gBound.height<<')'<<std::endl;
-            }
-            */
         }
         sf::Vector2i mousePosition = sf::Mouse::getPosition(windowMain);
         player.setPosition(sf::Vector2f(mousePosition.x - 24, mousePosition.y - 32));
 
-        if(NUM%200 == 0) {
+        if(NUM%5 == 0) {
             shot = false;
         }
-        if (!shot && sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+        if ( !shot && sf::Mouse::isButtonPressed(sf::Mouse::Left) ) {
             //TEMP playerBullet
-            Bullet* tmp =
-                new Bullet(
+            playerBullet.push_back(
+                Bullet(
                     sf::IntRect(69, 78, 9, 21),
                     sf::Vector2f(0.5f, 0.5f),
                     player.getPosition() + sf::Vector2f(24.f, -10.f),
-                    sf::Vector2f(0, -0.5)
-                    );
-            playerBullet.push_back(tmp);
+                    sf::Vector2f(0, -5)
+                    )
+                );
             shot = true;
         }
 
@@ -106,18 +88,18 @@ int main() {
         windowMain.draw(*(sf::Sprite*)&player);
 
         //Flash and draw all player's bullet
-        std::list<Bullet*>::iterator it;
-        for (it = playerBullet.begin(); it!=playerBullet.end();) {
-            (*it)-> flash();
-            if ((*it)-> isDisappear()) {
+        std::list<Bullet>::iterator it;
+        for ( it = playerBullet.begin(); it!=playerBullet.end(); ) {
+            it->flash();
+            if ( it->isDisappear() ) {
                 //Delete when disappear
-                delete (*it);
                 playerBullet.erase( it++ );
             } else {
-                windowMain.draw( *(sf::Sprite*)(*it) );
+                windowMain.draw( *it );
                 ++it;
             }
         }
+        std::cout<<playerBullet.size()<<std::endl;
 
 
         windowMain.display();
@@ -126,31 +108,3 @@ int main() {
 
     return 0;
 }
-
-//sf::Vector2f
-//getStepFromKeyboardEvent(
-//    sf::Event keyEvent
-//    )
-//{
-//    sf::Vector2f step;
-//
-//    switch(keyEvent.key.code) {
-//    case sf::Keyboard::Up :
-//        step = sf::Vector2f(0, -8);
-//        break;
-//
-//    case sf::Keyboard::Down :
-//        step = sf::Vector2f(0, 8);
-//        break;
-//
-//    case sf::Keyboard::Left :
-//        step = sf::Vector2f(-8, 0);
-//        break;
-//
-//    case sf::Keyboard::Right :
-//        step = sf::Vector2f(8, 0);
-//        break;
-//    }
-//
-//    return step;
-//}
--- src/Bullet.cpp
+++ src/Bullet.cpp
@@ -25,20 +25,5 @@ Bullet::flash( void )
 bool
 Bullet::isDisappear( void )
 {
-    int ans=0;
-    float top,bottom;
-    float left,right;
-
-    sf::FloatRect bound = this-> getGlobalBounds();
-
-    top = bound.top;
-    bottom = top + bound.height;
-    left = bound.left;
-    right = left + bound.width;
-
-    if ( left <0.f || 240.f < right ) return true;
-
-    if ( top < 0.f || 400.f < bottom ) return true;
-
-    return false;
- }
+    return ! this-> getGlobalBounds().intersects( sf::FloatRect(0.f, 0.f, 240.f, 400.f) );
+}
